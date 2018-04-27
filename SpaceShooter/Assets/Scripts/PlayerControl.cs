e3c69be231345e2a6ec5c466a41f8886ec0c45ee
AlexanderFranz <alexbalcombe4@gmail.com>
2018-04-27 17:31:29 +0800

    Shooting
    -Got some sort of shooting system in place not the best and still needs work.

---
+++ SpaceShooter/Assets/Scripts/PlayerControl.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControl : MonoBehaviour
+{
+
+    float speed = 10f;
+
+    public GameObject laserPrefab;
+    public GameObject leftTurret;
+    public GameObject rightTurret;
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Horizontal and Vertical are both set in the input settings under the edit tab.
+        // Horizontal is the Up/Down or W/S Keys.
+        float xAxis = Input.GetAxis("Horizontal");
+        // Vertical is the Left/Right or A/D Keys.
+        float yAxis = Input.GetAxis("Vertical");
+
+
+        transform.Translate(new Vector2(xAxis, yAxis) * Time.deltaTime * speed);
+
+        // Calling every frame if the spacebar is press.
+        if (Input.GetKeyDown("space"))
+        {
+            GameObject.Instantiate(laserPrefab, leftTurret.transform.position, Quaternion.identity);
+            GameObject.Instantiate(laserPrefab, rightTurret.transform.position, Quaternion.identity);
+        }
+
+    }
+
+}
---
+++ SpaceShooter/Assets/Scripts/PlayerLaser.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerLaser : MonoBehaviour
+{
+
+    public float laserSpeed = 0.2f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(transform.right * 0.2f);
+        // transform.Translate(transform.position.normalized * laserSpeed, Space.World);
+    }
+}
