fba882bb3828f3f862e1d8aece38d99a7d43fd75
AlexanderFranz <alexbalcombe4@gmail.com>
2018-04-28 20:40:09 +0800

    Progress
    -Currently changing some of the shooting system and expanding upon it.
    -UI for the Health and Energy bars for the player.
    -Going to start doing Health and Energy bar functions.
    -Added a temporary background.

--- SpaceShooter/Assets/Scripts/PlayerControl.cs
+++ SpaceShooter/Assets/Scripts/PlayerControl.cs
@@ -4,13 +4,24 @@ using UnityEngine;
 
 public class PlayerControl : MonoBehaviour
 {
-
+    // Player movement
     float speed = 10f;
-    bool shouldFire = false;
+    float startPosition = -8f;
 
+    // Player shooting
+    float weaponCoolDown = 1f;
+    float weaponEnergy = 100f;
+
+    // Game objects
     public GameObject laserBeam;
     public GameObject leftTurret;
     public GameObject rightTurret;
+
+    void Start()
+    {
+        // Starting the player position at the left side of the area.
+        transform.position = new Vector3(startPosition, 0);
+    }
 
     // Update is called once per frame
     void Update()
@@ -21,14 +32,14 @@ public class PlayerControl : MonoBehaviour
         // Horizontal is the Left/Right or A/D Keys.
         float yAxis = Input.GetAxis("Horizontal");
 
-
         transform.Translate(new Vector2(xAxis, yAxis) * Time.deltaTime * speed);
 
         // Calling every frame if the spacebar is press.
-        if (Input.GetButton("Jump"))
+        //if (Input.GetButton("Jump"))
+        if(Input.GetKeyDown("space"))
         {
-          Instantiate(laserBeam, leftTurret.transform.position, transform.rotation);
-          Instantiate(laserBeam, rightTurret.transform.position, transform.rotation);
+            Instantiate(laserBeam, leftTurret.transform.position, transform.rotation);
+            Instantiate(laserBeam, rightTurret.transform.position, transform.rotation);
         }
 
     }
--- SpaceShooter/Assets/Scripts/PlayerLaser.cs
+++ SpaceShooter/Assets/Scripts/PlayerLaser.cs
@@ -12,13 +12,13 @@ public class PlayerLaser : MonoBehaviour
         Vector3 position = transform.position;
 
         Vector3 velocity = new Vector3(0, Time.deltaTime * laserSpeed, 0);
-        //Vector3 velocity = new Vector3(0, Input.GetAxis("Vertical") * Time.deltaTime * laserSpeed, 0); Keep this code for now as it has cool effect.
+        // Vector3 velocity = new Vector3(0, Input.GetAxis("Vertical") * Time.deltaTime * laserSpeed, 0); Keep this code for now as it has cool effect.
 
         position += transform.rotation * velocity;
 
         transform.position = position;
 
-        //When the goes past 20 position from the original position that game object will destroy itself.
+        // When the goes past 20 positions from the original position that game object will destroy itself.
         if (transform.position.x > 20f)
         {
             Destroy(gameObject);
